9fd1c3991f7f0628ba14a4b244c738510591fb03
Abiola Fasanya <hezekiahfasanya@gmail.com>
2023-09-16 16:05:32 +0100

    Update options.js

--- src/data/options.js
+++ src/data/options.js
@@ -3,6 +3,6 @@ export const options = {
   headers: {
     accept: 'application/json',
     Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOGI0ZGZmNzUxNGVmNGQ0OGNjMDMwMzA5NDBlYzI0NiIsInN1YiI6IjYwY2MzYWNjYWFlYzcxMDAyOTZiMzhhYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.26Lkuzd2upChHV0yjiS5z1O7wEvQLKd27zkJQbcac1w',
+      'Bearer eyJhdWQiOiIyOGI0ZGZmNzUxNGVmNGQ0OGNjMDMwMzA5NDBlYzI0NiIsInN1YiI6IjYwY2MzYWNjYWFlYzcxMDAyOTZiMzhhYSIsIZXJzaW9uIjoxfQ.26Lkuzd2upChHV0yjiS5z1O7wEvQLKd27zkJQbcac1w',
   },
 };
